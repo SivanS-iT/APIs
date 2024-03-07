ff0d591c6e0dade44c3b1f0f8b04b426d73af58d
Ivan Sambol <ivan.sambol@br-automation.com>
2024-03-07 22:28:00 +0100

    First commit - prepare database

---
+++ SivanStore_API/SivanStore_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SivanStore_API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options) { }
+    }
+}
