31f1f6163fa3df72ce42b253d5ce6134c0027456
Ivan Sambol <ivan.sambol@br-automation.com>
2024-03-10 16:24:37 +0100

    Add blob service

--- SivanStore_API/SivanStore_API/Controller/MenuItemController.cs
+++ SivanStore_API/SivanStore_API/Controller/MenuItemController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SivanStore_API.Data;
+using SivanStore_API.Models;
+using System.Net;
 
 namespace SivanStore_API.Controller
 {
@@ -9,16 +11,42 @@ namespace SivanStore_API.Controller
     public class MenuItemController : ControllerBase
     {
         private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
          
         public MenuItemController(ApplicationDbContext db)
         {  
             _db = db; 
+            _response = new ApiResponse();
         }
 
         [HttpGet]
         public async Task<IActionResult> GetMenuItems()
         {
-            return Ok(_db.MenuItems);
+            _response.Result = _db.MenuItems;
+            _response.StatusCode = System.Net.HttpStatusCode.OK;
+            return Ok(_response);
         }
+
+        [HttpGet("{id:int}", Name = "GetMenuItem")]
+        public async Task<IActionResult> GetMenuItem(int id)
+        {
+            if (id == 0)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+            }
+            MenuItem menuItem = _db.MenuItems.FirstOrDefault(u => u.Id == id);
+            if (menuItem == null)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                _response.IsSuccess = false;
+                return NotFound(_response);
+            }
+            _response.Result = menuItem;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+
     }
 }
--- SivanStore_API/SivanStore_API/Program.cs
+++ SivanStore_API/SivanStore_API/Program.cs
@@ -1,12 +1,19 @@
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using RedMango_API.Services;
 using SivanStore_API.Data;
 using SivanStore_API.Models;
+using SivanStore_API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddSingleton(u => new BlobServiceClient(
+    builder.Configuration.GetConnectionString("StorageAccount")));
+builder.Services.AddSingleton<IBlobService, BlobService>();
+ 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
