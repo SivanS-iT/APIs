b9d41c209ef52cc6c05c6631ceae6c12c7281da0
Ivan Sambol <ivan.sambol1999@gmail.com>
2024-03-13 21:27:14 +0100

    Add autTest andpont and swagger bearer token

---
+++ SivanStore_API/SivanStore_API/Controller/AuthTestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SivanStore_API.Utility;
+
+namespace SivanStore_API.Controller
+{
+    [Route("api/AuthTest")]
+    [ApiController]
+    public class AuthTestController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<string>> GetAuth()
+        {
+            return "You are authenticated";
+        }
+
+        [HttpGet("{id:int}")]
+        [Authorize(Roles = Helper.Role_Admin)]
+        public async Task<ActionResult<string>> GetAuth(int authIntValue)
+        {
+            //authorization -> Authentication + Some access/roles
+            return "You are Authorized with Role of Admin";
+        }
+    }
+}
--- SivanStore_API/SivanStore_API/Program.cs
+++ SivanStore_API/SivanStore_API/Program.cs
@@ -1,10 +1,14 @@
 using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using RedMango_API.Services;
 using SivanStore_API.Data;
 using SivanStore_API.Models;
 using SivanStore_API.Services;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,9 +34,60 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.Password.RequireNonAlphanumeric = false;
 });
 
+
+var key = builder.Configuration.GetValue<string>("ApiSettings:Secret");
+builder.Services.AddAuthentication(u =>
+{
+    u.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    u.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(u =>
+{
+    u.RequireHttpsMetadata = false;
+    u.SaveToken = true;
+    u.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+builder.Services.AddCors(); // if an api is called from other URL
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description =
+            "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
+            "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+            "Example: \"Bearer 12345abcdef\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme = JwtBearerDefaults.AuthenticationScheme
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+           new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 
 var app = builder.Build();
@@ -45,6 +100,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(o => o.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().WithExposedHeaders("*"));
+
+// Needs to be in this order
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
