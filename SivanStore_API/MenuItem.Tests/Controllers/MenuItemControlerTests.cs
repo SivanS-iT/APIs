2aefbd1942f97b49704064115e63fb7516e68c41
Ivan Sambol <ivan.sambol1999@gmail.com>
2024-03-17 21:35:33 +0100

    Add ControllerUnit tests solution folder

---
+++ SivanStore_API/MenuItem.Tests/Controllers/MenuItemControlerTests.cs
@@ -0,0 +1,43 @@
+﻿using RedMango_API.Services;
+using SivanStore_API.Data;
+using SivanStore_API.Models;
+using FakeItEasy;
+
+using SivanStore_API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SivanStore_API.Tests.Controllers
+{
+    public class MenuItemControlerTests
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IBlobService _blobService;
+        private ApiResponse _response;
+        public MenuItemControlerTests() 
+        {
+            _db = A.Fake<ApplicationDbContext>();
+            _blobService = A.Fake<IBlobService>();
+            _response = new ApiResponse();
+        }
+
+
+        [Fact]
+        public void ManuItemController_GetMenuItems_ReturnOK()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+
+
+
+
+}
---
+++ SivanStore_API/MenuItem.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MenuItem.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
